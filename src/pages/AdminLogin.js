35ac64b06749f021ca8b75720b098a9022d789bb
Ben-LK6 <bloko3565@gmail.com>
2025-10-20 17:26:34 +0100

    new commit 19

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -13,7 +13,7 @@ const AdminLogin = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:8000/api/auth.php', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL || 'http://localhost:8000/api'}/auth.php`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
